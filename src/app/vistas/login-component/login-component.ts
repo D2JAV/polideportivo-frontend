9f0afea373210bcf32b38e3587be8c7213badd5e
ghinohuaman12 <ghinohuaman39@gmail.com>
2025-11-11 12:57:16 -0500

    Se agrego la estructura a login-component

--- src/app/vistas/login-component/login-component.ts
+++ src/app/vistas/login-component/login-component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+
+  ],
   templateUrl: './login-component.html',
-  styleUrl: './login-component.css',
+  styleUrls: ['./login-component.css'],  
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  rememberMe: boolean = false
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (!this.username.trim() || !this.password.trim()) {
+      alert('Por favor, completa usuario y contraseña');
+      return;
+    }
+
+    console.log('Login exitoso:', { username: this.username, rememberMe: this.rememberMe });
+
+    
+    this.router.navigate(['/dashboard']);
+  }
 }
